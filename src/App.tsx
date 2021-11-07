7d83175d36a54d26a8457d01a968f5518545c785
vihong <probamaths@gmail.com>
2021-11-07 20:01:42 +0100

    ✅ F10: Update tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import logo from "./logo.svg"
 import "./App.css"
 import { useState } from "react"
+import { Router } from "@reach/router"
+import Order from "./components/pages/Order"
+import Login from "./components/pages/Login"
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">bonjour</header>
-    </div>
+    <Router>
+      <Order path="/" />
+      <Login path="/login" />
+    </Router>
   )
 }
-
-export default App
