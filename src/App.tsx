93aee2e2c7ae49af96f268375299f72bc80a876e
vihong <probamaths@gmail.com>
2021-11-23 20:50:31 +0100

    🚀 F51: Fix imports to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Router } from "@reach/router"
-import Order from "./components/pages/order/Order"
-import Login from "./components/pages/Login"
+import Order from "components/pages/order/Order"
+import Login from "components/pages/Login"
 
 export default function App() {
   return (
