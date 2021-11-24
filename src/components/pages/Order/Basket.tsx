11bc2dfd73c300e193b909d2ef98481bb161aec8
vihong <probamaths@gmail.com>
2021-11-24 12:16:00 +0100

    🚧 F15: BASKET > add key static elements

--- src/components/pages/Order/Basket.tsx
+++ src/components/pages/Order/Basket.tsx
@@ -2,7 +2,18 @@ import React from "react"
 import styled from "styled-components/macro"
 
 export default function Basket() {
-  return <BasketStyled>Basket</BasketStyled>
+  return (
+    <BasketStyled>
+      <h1>Votre commande :</h1>
+      <h2>Total</h2>
+      <ul className="products-added">
+        <li>product 1</li>
+        <li>product 2</li>
+      </ul>
+      {/* <button>Payer</button> */}
+      {/* <button>Payer</button> */}
+    </BasketStyled>
+  )
 }
 
 const BasketStyled = styled.div`
