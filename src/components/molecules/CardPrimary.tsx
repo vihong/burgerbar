65da7ed215e76c93b1eace2d03f0bd639bbba961
vihong <probamaths@gmail.com>
2021-12-01 12:01:51 +0100

    🚧 F19: MENU > CARDS > fix overall size and add image container with padding

--- src/components/molecules/CardPrimary.tsx
+++ src/components/molecules/CardPrimary.tsx
@@ -32,7 +32,9 @@ export default function CardPrimary(props: CardPrimaryProps) {
   return (
     <CardStyled onClick={onCardClick} className={isHoverable ? "is-hoverable" : ""}>
       {hasDeleteButton && <Button label={"X"} className="delete-button" onClick={onDeleteButton} />}
-      <img className="image" src={!imageSource ? IMAGE_BY_DEFAULT : imageSource} alt={title} />
+      <div className="image">
+        <img src={!imageSource ? IMAGE_BY_DEFAULT : imageSource} alt={title} />
+      </div>
 
       <div className="card-text">
         <span className="card-title">{title}</span>
@@ -50,11 +52,11 @@ const CardStyled = styled.div`
   width: 200px;
   height: 300px;
   border-radius: 5px;
-  padding: 50px 20px 20px; // padding top to avoid click conflict with delete button
+  padding: 20px 20px 10px; // padding top to avoid click conflict with delete button
   box-shadow: 0 0 8px 0 rgb(0 0 0 / 20%);
   display: grid;
-  grid-template-rows: 60% 1fr;
-  grid-gap: 20px;
+  grid-template-rows: 65% 1fr;
+  grid-gap: 10px;
   /* flex-direction: column; */
   /* justify-content: center; */
   /* align-items: center; */
@@ -71,17 +73,17 @@ const CardStyled = styled.div`
 
   .image {
     border: 2px solid green;
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-    position: relative;
-    grid-row: 1 / 2;
+    padding: 0.5em;
+    margin-top: 10px;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
   }
 
   .card-text {
     border: 1px solid yellow;
-    grid-row: 2/ 3;
-    width: 100%;
     /* display: flex; */
     /* flex-direction: column; */
     /* justify-content: flex-end; */
