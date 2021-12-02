3a4095315b13b37a7c86eeb5939b464af80861a2
vihong <probamaths@gmail.com>
2021-12-03 00:23:17 +0100

    ♻️ F61: PANEL ADMIN > WindowTab > is now generic

--- src/components/atoms/WindowTab.tsx
+++ src/components/atoms/WindowTab.tsx
@@ -1,54 +1,33 @@
 import React from "react"
-import { FiChevronUp, FiChevronDown } from "react-icons/fi"
 import styled from "styled-components"
 import { theme } from "theme"
 
 interface PanelTabProps {
-  isClosed?: boolean
+  label?: string
+  IconComponent?: JSX.Element
   onClick?: any
+  className?: string
 }
 
-export default function WindowTab({ isClosed, onClick }: PanelTabProps) {
+export default function WindowTab({ label, IconComponent, onClick, className }: PanelTabProps) {
   return (
-    <PanelTabStyled onClick={onClick}>
-      {isClosed ? <FiChevronUp className="icon" /> : <FiChevronDown className="icon" />}
-      <span>{isClosed ? "Ouvrir" : "Réduire"}</span>
+    <PanelTabStyled onClick={onClick} className={className}>
+      {IconComponent}
+      <span>{label}</span>
     </PanelTabStyled>
   )
 }
 
 const PanelTabStyled = styled.div`
-  border: 1px solid ${theme.colors.incognito};
-  position: absolute;
-  top: -36px;
-  background: ${theme.colors.incognito};
+  /* border: 1px solid ${theme.colors.incognito}; */
   color: ${theme.colors.white};
   display: inline-flex;
   align-items: center;
-  margin-left: 60px;
-  min-height: 35px;
+  height: 35px;
   border-radius: ${theme.borderRadius.round};
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
   border-bottom: ${theme.colors.background_white};
-  min-width: 100px;
-  padding-left: 0.5em;
-  padding-right: 0.4em;
-
-  :hover {
-    cursor: pointer;
-    background: ${theme.colors.white};
-    color: ${theme.colors.incognito};
-    border: 1px solid ${theme.colors.greyLight};
-    border-bottom: none;
-  }
-
-  span {
-    margin: auto;
-  }
-
-  .icon {
-    min-width: 1.5em;
-    min-height: 1.5em;
-  }
+  padding: 0 16px;
+  cursor: pointer;
 `
--- src/components/pages/Order/PanelAdmin/PanelAdmin.tsx
+++ src/components/pages/Order/PanelAdmin/PanelAdmin.tsx
@@ -3,13 +3,24 @@ import { useContext } from "react"
 import styled from "styled-components/macro"
 import WindowTab from "components/atoms/WindowTab"
 import PanelWindow from "components/pages/Order/PanelAdmin/PanelWindow"
+import { FiChevronDown, FiChevronUp } from "react-icons/fi"
+import { theme } from "theme"
 
 export default function PanelAdmin() {
   const { isCollapsed, setIsCollapsed } = useContext(OrderContext)
 
   return (
     <PanelAdminStyled className="panel">
-      <WindowTab isClosed={isCollapsed} onClick={() => setIsCollapsed(!isCollapsed)} />
+      <div className="panel-tabs">
+        <WindowTab
+          isClosed={isCollapsed}
+          labelOnOpened="Réduire"
+          IconComponentOnOpened={<FiChevronDown className="icon" />}
+          labelOnClosed="Ouvrir"
+          IconComponentOnClosed={<FiChevronUp className="icon" />}
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        />
+      </div>
       {!isCollapsed && <PanelWindow />}
     </PanelAdminStyled>
   )
@@ -20,4 +31,10 @@ const PanelAdminStyled = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
+
+  .panel-tabs {
+    position: absolute;
+    top: -36px;
+    /* border: 1px solid red; */
+  }
 `
