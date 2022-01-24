071ab647bd7b81c9bcc23e07c66ea6f5507aee0d
vihong <probamaths@gmail.com>
2022-01-24 13:39:17 +0100

    ✨ F82: NAVBAR > Add mode admin toast back

--- src/components/pages/Order/Navbar/NavBarRightSide.tsx
+++ src/components/pages/Order/Navbar/NavBarRightSide.tsx
@@ -6,11 +6,27 @@ import { theme } from "theme"
 import SwitchButton from "components/atoms/SwitchButton"
 import Profile from "components/molecules/Profile"
 import ToggleButton from "components/atoms/Buttons/ToggleButton"
+import toast from "react-hot-toast"
 
 export default function NavBarRightSide() {
   const { isModeAdmin, setIsModeAdmin } = useContext(OrderContext)
 
   const toggleButtonAdmin = () => {
+    !isModeAdmin &&
+      toast("Astuce : cliquez sur un produit pour le modifier", {
+        position: "top-center",
+        duration: 5000,
+        style: {
+          // borderRadius: theme.borderRadius.round,
+          borderRadius: "10px",
+          background: "#333",
+          color: "#fff",
+          position: "relative",
+          top: "8vh",
+          right: "-550px",
+          width: "250px",
+        },
+      })
     setIsModeAdmin(!isModeAdmin)
   }
 
