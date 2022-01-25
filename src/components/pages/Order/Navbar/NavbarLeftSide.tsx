590931e0fecb86585305999ee227e8fb432d6834
vihong <probamaths@gmail.com>
2022-01-25 20:48:31 +0100

    ✨ F83: NAVBAR > Change logo icon

--- src/components/pages/Order/Navbar/NavbarLeftSide.tsx
+++ src/components/pages/Order/Navbar/NavbarLeftSide.tsx
@@ -5,7 +5,7 @@ import { theme } from "theme"
 export default function NavBarLeftSide() {
   return (
     <NavBarLeftSideStyled onClick={() => window.location.reload()}>
-      <img src="burger-icon.png" alt="burger-icon" />
+      <img src="images/logo-burger.png" alt="burger-icon" />
       <h1>Burger Live</h1>
     </NavBarLeftSideStyled>
   )
@@ -23,7 +23,7 @@ const NavBarLeftSideStyled = styled.div`
     /* border: 1px solid green; */
     object-fit: contain;
     object-position: center;
-    height: 6vh;
+    height: 4.5vh;
     width: 100px; // for Safari and Firefox
   }
   h1 {
@@ -32,9 +32,10 @@ const NavBarLeftSideStyled = styled.div`
     width: 100%;
     text-align: center;
     color: ${theme.colors.primary};
-    margin-left: 0px;
+    margin-left: 5px;
     font-size: ${theme.fonts.P5};
     text-transform: uppercase;
     font-family: "Gidugu", sans-serif;
+    letter-spacing: 1.5px;
   }
 `
